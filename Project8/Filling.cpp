3ed0f77f33146f62c6455ae2556b2c8138fd4d40
S3xualiz3r <r3885585@gmail.com>
2023-12-18 16:05:06 +0300

    чтото сделал

--- Project8/Filling.cpp
+++ Project8/Filling.cpp
@@ -1,24 +1,49 @@
-#include <iostream>
-#include "functions.h"
-
-using namespace std;
-
-void filling() {
-	extern int matrix[5][2];//mkmcekcodmceokcmeocmkodmeocdmedcoecdmo
-
-	int num1, num2;
-	while (true)
-	{
-		cin >> num1 >> num2;
-		num1--;
-		num2--;
-
-		if (matrix[num1][0] == matrix[num2][1]) {
-			matrix[num1][0] = 0;
-			matrix[num2][1] = 0;
-		}
-		else
-			cout << "Совпадений не найдено." << endl;
-
-	}
+#include <iostream> 
+#include "functions.h" 
+#include <cstdlib> 
+#include <ctime> 
+ 
+ 
+ 
+using namespace std; 
+ 
+void filling() { 
+ srand(time(0)); 
+ extern int matrix[5][2];//mkmcekcodmceokcmeocmkodmeocdmedcoecdmo 
+ 
+ int num1, num2; 
+ bool  
+  first = true, 
+  random; 
+ 
+ int num = 0; 
+ for (int i = 0; i < 5; i++) 
+  if (matrix[i][0] == 0) 
+   num++; 
+ 
+ if (num > 1) { 
+  num1 = rand() % 100 + 1; 
+  num2 = rand() % 100 + 1; 
+  random = (rand() & 1); 
+ 
+  for (int i = 0; i < 5; i++) 
+   if(matrix[i][0] == 0) 
+    if(first) { 
+     matrix[i][0] = num1; 
+     first = false; 
+    } 
+    else  
+     matrix[i][0] = num2; 
+ 
+  first = true; 
+ 
+  for (int i = 0; i < 5; i++) 
+   if (matrix[i][1] == 0) 
+    if (first) { 
+     matrix[i][1] = (random)? num1 : num2; 
+     first = false; 
+    } 
+    else 
+     matrix[i][1] = (random) ? num2 : num1; 
+ } 
 }
